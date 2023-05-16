228d58e1f86b15f09b8722263e9411e87d594dae
zloebok6 <54581698+zloebok6@users.noreply.github.com>
2023-05-16 23:46:34 +0300

    web application loading

--- src/main/java/com/zloebok/todoproject/model/ToDoItem.java
+++
@@ -1,7 +0,0 @@
-package com.zloebok.todoproject.model;
-
-import lombok.Data;
-
-@Data
-public class ToDoItem {
-}
--- src/main/java/com/zloebok/todoproject/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zloebok.todoproject.repository;
-
-import com.zloebok.todoproject.entity.ToDoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ToDoRepository extends JpaRepository<ToDoEntity, Long> {
-}
